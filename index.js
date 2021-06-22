d4f3b508a52519702b68f13da9960315b3103269
rdwentmp <maaarek7823@gmail.com>
2021-06-22 11:46:40 +0200

    Resolve promise

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const fetchData = async (searchTerm) => {
     
     const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-    apikey: 'cf3f4637',
-    s: searchTerm
+        params: {
+        apikey: 'cf3f4637',
+        s: searchTerm
+        }
+    });
+    if (response.data.Error) {
+        return [];
     }
-});
-console.log(response.data);
+    return response.data.Search;
 };
 
 const input = document.querySelector('input');
 
-const onInput = debounce(event => {
-    fetchData(event.target.value);
-});
+const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+    
+    for (let movie of movies) {
+        const div = document.createElement('div');
+
+        div.innerHTML = `
+            <img src="${movie.Poster}" />
+            <h1>${movie.Title}<h1/>
+        `;
+
+        document.querySelector('#target').appendChild(div);
+    }
+
+
+};
 input.addEventListener('input', debounce(onInput, 500));
