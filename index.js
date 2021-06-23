41b6ccb7b34456093de514fcea2ef0fb278ffd99
rdwentmp <maaarek7823@gmail.com>
2021-06-23 09:29:19 +0200

    closing dropdown

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
-const fetchData = async (searchTerm) => {
-    
-    const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-        apikey: 'cf3f4637',
-        s: searchTerm
-        }
-    });
-    if (response.data.Error) {
-        return [];
+const fetchData = async searchTerm => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'd9835cc5',
+      s: searchTerm
     }
-    return response.data.Search;
+  });
+
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
 };
 
 const root = document.querySelector('.autocomplete');
 root.innerHTML = `
-    <label><b>Search for the movie</b></label>
-    <input class="input" />
-    <div class="dropdown">
-      <div class="dropdown-menu">
-        <div class="dropdown-content results"></div>
-      </div>
+  <label><b>Search For a Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
     </div>
-    `;
+  </div>
+`;
 
 const input = document.querySelector('input');
 const dropdown = document.querySelector('.dropdown');
 const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
-    const movies = await fetchData(event.target.value);
-    
-    for (let movie of movies) {
-        const div = document.createElement('div');
+  const movies = await fetchData(event.target.value);
 
-        div.innerHTML = `
-            <img src="${movie.Poster}" />
-            <h1>${movie.Title}<h1/>
-        `;
+  resultsWrapper.innerHTML = '';
+  dropdown.classList.add('is-active');
+  for (let movie of movies) {
+    const option = document.createElement('a');
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
-        document.querySelector('#target').appendChild(div);
-    }
+    option.classList.add('dropdown-item');
+    option.innerHTML = `
+      <img src="${imgSrc}" />
+      ${movie.Title}
+    `;
 
-
+    resultsWrapper.appendChild(option);
+  }
 };
 input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', event => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove('is-active');
+  }
+});
